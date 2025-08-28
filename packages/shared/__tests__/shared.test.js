3c01626fc72d957be6991272a0c4c5e37108660f
Ebenezer Igbinoba <ebenezer.igbinoba@stu.cu.edu.ng>
2025-08-28 17:43:34 +0100

    changed shared test

--- packages/shared/__tests__/shared.test.js
+++ packages/shared/__tests__/shared.test.js
@@ -1,5 +1,6 @@
 import { greet } from '../lib/shared.js';
 import { strict as assert } from 'assert';
 
+
 assert.strictEqual(greet("assets"), "Hello, assets!");
 console.info("assets tests passed");
